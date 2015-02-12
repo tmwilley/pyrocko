58d83fd41733b42badc990a3613b9deff3c89d3a
Sebastian Heimann <sebastian.heimann@gfz-potsdam.de>
2015-02-12 22:32:22 +0100

    modified response tests to use external data files

--- test/test_response.py
+++ test/test_response.py
@@ -2,6 +2,8 @@ import unittest, os
 import numpy as num
 from pyrocko import util, evalresp, pz, trace, guts
 
+import common
+
 def numeq(a,b, eps):
     return num.all(num.abs(num.array(a) - num.array(b)) < eps)
 
@@ -9,7 +11,7 @@ class ResponseTestCase(unittest.TestCase):
     
     def test_evalresp(self, plot=False):
 
-        testdir = os.path.dirname(__file__)
+        resp_fpath = common.test_data_file('test2.resp')
 
         freqs = num.logspace(num.log10(0.001), num.log10(10.), num=1000)
 
@@ -20,13 +22,12 @@ class ResponseTestCase(unittest.TestCase):
                           instant=util.str_to_time('2012-01-01 00:00:00'),
                           freqs=freqs,
                           units='DIS',
-                          file=os.path.join(testdir, 'response', 'RESP.GR.BSEG..BHZ'),
+                          file=resp_fpath,
                           rtype='CS')[0][4]
 
-        pzfn = 'SAC_PZs_GR_BSEG_BHZ__2008.254.00.00.00.0000_2599.365.23.59.59.99999'
+        pz_fpath = common.test_data_file('test2.sacpz')
 
-        zeros, poles, constant = pz.read_sac_zpk(filename=os.path.join(
-            testdir, 'response', pzfn))
+        zeros, poles, constant = pz.read_sac_zpk(pz_fpath)
         
         resp = trace.PoleZeroResponse(zeros, poles, constant)
 
